9f784d03dc8f599254c2da5727faed750b0f1630
oluwajuwon <jfagbohungbe@gmail.com>
2018-08-16 07:18:17 +0100

    made changes to the about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,7 @@ class about extends Component {
                             <a href="mailto:jfagbohungbe@gmail.com" className="mr-2"><FontAwesomeIcon icon="envelope" /></a>
                         </div>
                         <div className="col-md-8">
-                            <p className="textcolor1"><b>I am currently a Quality Assurance analyst at Rescue Technologies</b></p>
+                            <p className="textcolor1"><b>I am a developer currently on a journey to discover new technologies and methods of proferring solutions</b></p>
                             <p>As a Budding Developer, my main goal is to solve problems, create solutions and learn new methods of solving different kinds of problems, regardless of technology or programming language used. The main goal is to <b className="textcolor1">SOLVE PROBLEMS</b>. With a background in design, I love clean, minimal designs that fulfils its function while still aesthetically pleasing. </p>
                             <p>I have used a few programming languages and tools over the years and i've come to enjoy the new wave JavaScript is making and how it has planned to change the face of software development all over the globe.</p>
                             <p>I do also love scouring dribbble for real great designs and also creating my own mockups and prototypes on Adobe XD.</p>
